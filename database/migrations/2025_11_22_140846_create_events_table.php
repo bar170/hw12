46ca96a58cfe5d0c950698ed23fc7fa80a29a5a3
Grigoriev.E <grigorievegor.ru@gmail.com>
2025-11-22 17:21:44 +0300

    Миграции

---
+++ database/migrations/2025_11_22_140846_create_events_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('driver_id');
+            $table->unsignedBigInteger('transport_id');
+            $table->unsignedBigInteger('route_id');
+            $table->timestamps();
+
+            $table->foreign('driver_id')
+                ->references('id')
+                ->on('drivers')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            $table->foreign('transport_id')
+                ->references('id')
+                ->on('transports')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            $table->foreign('route_id')
+                ->references('id')
+                ->on('routes')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
